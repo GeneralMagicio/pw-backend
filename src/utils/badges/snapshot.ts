a934a2bcaeb8ec01cd41396528ce2ba89d19702f
Mahdi <mahdighajary@gmail.com>
2024-07-07 15:28:39 +0330

    Update the snapshot file

--- src/utils/badges/snapshot.ts
+++ src/utils/badges/snapshot.ts
@@ -1,5 +1,7 @@
 import { productionSnapshots } from './production-snapshot';
 import { testAddresses } from './test-snapshots';
 
-
-export const snapshots = process.env.NODE_ENV === "production" ?  [...productionSnapshots] : [...productionSnapshots, ...testAddresses] 
+export const snapshots =
+  process.env.NODE_ENV === 'production'
+    ? [...productionSnapshots]
+    : [...productionSnapshots, ...testAddresses];
--- src/utils/badges/snapshotQa.ts
+++ src/utils/badges/snapshotQa.ts
@@ -1,45 +1,89 @@
-/* 
+// import { RawSnapshotPoint } from './type';
+// import { productionSnapshots } from './production-snapshot2';
+// import { snapshots707 } from './snapshots707';
+// import * as fs from 'fs'
+// import { PrismaClient } from '@prisma/client';
+// import { removeds } from './removeds';
 
-import { RawSnapshotPoint, productionSnapshot } from './snapshot';
-import { snapshots } from './snapshot703';
+// // function compareSnapshots(
+// //   snapshot1: RawSnapshotPoint,
+// //   snapshot2: RawSnapshotPoint,
+// // ): boolean {
+// //   // Iterate over the keys of the first snapshot
+// //   for (const key in snapshot1) {
+// //     // Compare the current key's value in both snapshots
+// //     // Convert values to string to handle number vs string comparison correctly
+// //     const value1 = snapshot1[key as keyof RawSnapshotPoint];
+// //     const value2 = snapshot2[key as keyof RawSnapshotPoint];
 
-function compareSnapshots(
-  snapshot1: RawSnapshotPoint,
-  snapshot2: RawSnapshotPoint,
-): boolean {
-  // Iterate over the keys of the first snapshot
-  for (const key in snapshot1) {
-    // Compare the current key's value in both snapshots
-    // Convert values to string to handle number vs string comparison correctly
-    const value1 = snapshot1[key as keyof RawSnapshotPoint];
-    const value2 = snapshot2[key as keyof RawSnapshotPoint];
+// //     if (String(value1) !== String(value2)) {
+// //       // Values differ, return true
+// //       return true;
+// //     }
+// //   }
 
-    if (String(value1) !== String(value2)) {
-      // Values differ, return true
-      return true;
-    }
-  }
+// //   // No differences found, return false
+// //   return false;
+// // }
 
-  // No differences found, return false
-  return false;
-}
+// function findRepetitions(
+//   snapshots: RawSnapshotPoint[],
+//   snapshots2: RawSnapshotPoint[],
+// ) {
+//   // 764
+//   let count = 0;
+//   let countNotR = 0;
+//   const addresses : string[] = [];
+//   snapshots.forEach((el, index) => {
+//     const exists = snapshots2.findIndex(
+//       (item, i2) => item.User.toLowerCase() === el.User.toLowerCase(),
+//     );
 
-function findRepetitions(
-  snapshots: RawSnapshotPoint[],
-  snapshots2: RawSnapshotPoint[],
-) {
-  let count = 0
-  snapshots.forEach((el, index) => {
-    const exists = snapshots2.findIndex(
-      (item, i2) => item.User === el.User,
-    );
+//     if (exists === -1) {
+//       // console.log('Count:', count++);
+//       // console.log('Item found', el);
+//       addresses.push(el.User);
+//       if (
+//         Number(el.badgeholderPoints) > 0 ||
+//         el.delegatePoints > 0 ||
+//         el.holderPoints > 0
+//       )
+//         countNotR++;
+//     }
+//   });
 
-    if (exists === -1) {
-      console.log("Item found", count++)
-    }
-  });
-}
+//   console.log('Not recepients', countNotR);
+//   console.log('Addresses', addresses);
+// }
 
-findRepetitions(snapshots, productionSnapshot);
+// findRepetitions(productionSnapshots, snapshots707)
 
-*/
+
+// // export const findRemovedRecipients = async () => {  
+// //     const prisma = new PrismaClient({
+// //       datasources: {
+// //         db: {
+// //           url: process.env.POSTGRES_PRISMA_URL,
+// //         },
+// //       },
+// //     });
+  
+// //     await prisma.$connect();
+
+// //     const users = await prisma.user.findMany({
+// //       where: {
+// //         opAddress: {
+// //           in: removeds,
+// //         }
+// //       }
+// //     })
+
+// //     console.log(users)
+  
+   
+  
+// //     await prisma.$disconnect();
+  
+// // }
+
+// // findRemovedRecipients()
