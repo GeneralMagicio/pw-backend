723f83e9a035033e942254e72a1297b296d43b00
Mahdi <mahdighajary@gmail.com>
2024-07-29 14:31:30 +0330

    Add rpgf4 analytics endpoints

--- src/analytics/analytics.controller.ts
+++ src/analytics/analytics.controller.ts
@@ -1,112 +1,232 @@
 import { Controller, Get, Logger, Res } from '@nestjs/common';
-
-import { AnalyticsService } from './analytics.service';
 import { PrismaService } from 'src/prisma.service';
 import { Response } from 'express';
-import { FlowService } from 'src/flow/flow.service';
-import { BHs } from './badgeholderts';
+import { BadgeData } from 'src/utils/badges/readBadges';
 
 @Controller({ path: 'analytics' })
 export class AnalyticsController {
   private readonly logger = new Logger(AnalyticsController.name);
-  private badgeholders = BHs.map((item) => item.recipient);
-  constructor(
-    private readonly analyticsService: AnalyticsService,
-    private readonly prismaService: PrismaService,
-    private readonly flowService: FlowService,
-  ) {}
+  // private badgeholders = BHs.map((item) => item.recipient);
+  constructor(private readonly prismaService: PrismaService) {}
 
-  @Get('users')
+  @Get('summary')
   async countTotalUsers(@Res() res: Response) {
-    const users = await this.prismaService.user.findMany({
-      distinct: ['address'],
-    });
-    const addresses = users.map((item) => item.address);
-    const bhs = addresses.filter((val) => this.badgeholders.includes(val));
+    const users = await this.prismaService.user.findMany({});
 
-    return res.send(
-      `Number of users as of ${new Date().toUTCString()}: ${users.length}<br/>
-      Number of badgeholders as of ${new Date().toUTCString()}: ${bhs.length}
-      `,
+    console.log('Total # of users', users.length);
+
+    const sHTemp = users.filter(
+      (user) =>
+        user.badges &&
+        user.identity &&
+        typeof user.badges === 'object' &&
+        Object.keys(user.badges).length > 0,
     );
-  }
 
-  @Get('lists')
-  async countTotalLists(@Res() res: Response) {
-    const lists = await this.prismaService.userAttestation.findMany({
-      include: {
-        user: true,
+    console.log('Total # of stakeholders:', sHTemp.length);
+
+    const guests = users.filter(
+      (user) =>
+        user.badges &&
+        user.identity &&
+        typeof user.badges === 'object' &&
+        typeof user.identity === 'object' &&
+        Object.keys(user.badges).length === 0 &&
+        Object.keys(user.identity).length === 0,
+    );
+
+    console.log(
+      'Total # of guests (No wallet connected but interacted with the app):',
+      guests.length,
+    );
+
+    const noInteractions = users.filter(
+      (user) => !user.badges && !user.identity,
+    );
+
+    console.log(
+      'Total # of users that had no interaction after logging:',
+      noInteractions.length,
+    );
+
+    const stakeholders = sHTemp.map(({ badges, ...el }) => ({
+      ...el,
+      badges: badges?.valueOf() as BadgeData,
+    }));
+
+    const recipients = stakeholders.filter(
+      (el) => el.badges.recipientsPoints && el.badges.recipientsPoints > 0,
+    );
+
+    const holders = stakeholders.filter(
+      (el) => el.badges.holderPoints && el.badges.holderPoints > 0,
+    );
+
+    const badgeHolders = stakeholders.filter(
+      (el) => el.badges.badgeholderPoints && el.badges.badgeholderPoints > 0,
+    );
+
+    const delegates = stakeholders.filter(
+      (el) => el.badges.delegatePoints && el.badges.delegatePoints > 0,
+    );
+
+    const recipientAttests = await this.prismaService.userAttestation.findMany({
+      where: {
+        userId: { in: recipients.map((el) => el.id) },
       },
     });
 
-    const addresses = lists.map((list) => list.user.address);
-    const bhs = addresses.filter((val) => this.badgeholders.includes(val));
-    // const bhs = addresses.filter((val) => this.badgeholders.includes(val));
-
-    return res.send(
-      `
-      Number of created lists as of ${new Date().toUTCString()}: ${
-        lists.length
-      }<br/>
-      Number of created lists by badgeholders as of ${new Date().toUTCString()}: ${
-        bhs.length
-      }
-      
-      `,
-    );
-  }
-
-  @Get('unlockeds')
-  async countTotalUnlockeds(@Res() res: Response) {
-    const users = await this.prismaService.user.findMany();
-
-    const finishedCollections =
-      await this.prismaService.userCollectionFinish.findMany({
-        include: {
-          user: true,
+    const holderAttestations =
+      await this.prismaService.userAttestation.findMany({
+        where: {
+          userId: { in: holders.map((el) => el.id) },
         },
       });
 
-    const addresses = users.map((item) => item.address);
-    const bhs = addresses.filter((val) => this.badgeholders.includes(val));
+    const badgeHolderAttestations =
+      await this.prismaService.userAttestation.findMany({
+        where: {
+          userId: { in: badgeHolders.map((el) => el.id) },
+        },
+      });
 
-    const collectionAddresses = finishedCollections.map(
-      (item) => item.user.address,
-    );
-    const bhsUnlockeds = collectionAddresses.filter((el) =>
-      this.badgeholders.includes(el),
-    );
-    // const numOfTrues = lists.filter((el) => el === true).length;
+    const delegateAttestations =
+      await this.prismaService.userAttestation.findMany({
+        where: {
+          userId: { in: delegates.map((el) => el.id) },
+        },
+      });
 
-    return res.send(
-      `Number of unlocked areas as of ${new Date().toUTCString()}: ${
-        users.length + finishedCollections.length
-      }<br/>
-      Number of unlocked areas by badgeholders as of ${new Date().toUTCString()}: ${
-        bhs.length + bhsUnlockeds.length
-      }
-      
-      `,
+    const recipientCategories =
+      await this.prismaService.userCollectionFinish.findMany({
+        where: { userId: { in: recipients.map((el) => el.id) } },
+      });
+
+    const holderCategories =
+      await this.prismaService.userCollectionFinish.findMany({
+        where: { userId: { in: holders.map((el) => el.id) } },
+      });
+
+    const badgeHolderCategories =
+      await this.prismaService.userCollectionFinish.findMany({
+        where: { userId: { in: badgeHolders.map((el) => el.id) } },
+      });
+
+    const delegateCategories =
+      await this.prismaService.userCollectionFinish.findMany({
+        where: { userId: { in: delegates.map((el) => el.id) } },
+      });
+
+    console.log('Number of users with the recipient badge:', recipients.length);
+    console.log('Number of category completed:', recipientCategories.length);
+    console.log('Number of their votes:', recipientAttests.length);
+
+    console.log();
+
+    console.log('Number of users with the holder badge:', holders.length);
+    console.log('Number of category completed:', holderCategories.length);
+    console.log('Number of their votes:', holderAttestations.length);
+
+    console.log();
+
+    console.log(
+      'Number of users with the badge-holder badge:',
+      badgeHolders.length,
     );
+    console.log('Number of category completed:', badgeHolderCategories.length);
+    console.log('Number of their votes:', badgeHolderAttestations.length);
+
+    console.log();
+
+    console.log('Number of users with the delegate badge:', delegates.length);
+    console.log('Number of category completed:', delegateCategories.length);
+    console.log('Number of their votes:', delegateAttestations.length);
+
+    return 'Success';
   }
 
-  @Get('votes')
-  async countTotalVotes(@Res() res: Response) {
-    const votes = await this.prismaService.vote.findMany({
-      include: { user: true },
+  @Get('cat-sum')
+  async categoryBasedVotes(@Res() res: Response) {
+    const users = await this.prismaService.user.findMany({});
+    const categories = await this.prismaService.project.findMany({
+      select: { id: true, name: true },
+      where: { type: 'collection' },
     });
 
-    const addresses = votes.map((item) => item.user.address);
-    const bhs = addresses.filter((el) => this.badgeholders.includes(el));
-    return res.send(
-      `
-      Number of pairwise votes as of ${new Date().toUTCString()}: ${
-        votes.length
-      }<br/>
-      Number of pairwise votes by badgeholders as of ${new Date().toUTCString()}: ${
-        bhs.length
-      }
-      `,
+    const sHTemp = users.filter(
+      (user) =>
+        user.badges &&
+        user.identity &&
+        typeof user.badges === 'object' &&
+        Object.keys(user.badges).length > 0,
     );
+
+    const stakeholders = sHTemp.map(({ badges, ...el }) => ({
+      ...el,
+      badges: badges?.valueOf() as BadgeData,
+    }));
+
+    const recipients = stakeholders.filter(
+      (el) => el.badges.recipientsPoints && el.badges.recipientsPoints > 0,
+    );
+
+    const holders = stakeholders.filter(
+      (el) => el.badges.holderPoints && el.badges.holderPoints > 0,
+    );
+
+    const badgeHolders = stakeholders.filter(
+      (el) => el.badges.badgeholderPoints && el.badges.badgeholderPoints > 0,
+    );
+
+    const delegates = stakeholders.filter(
+      (el) => el.badges.delegatePoints && el.badges.delegatePoints > 0,
+    );
+
+    const targetDate = new Date(Date.UTC(2024, 6, 17, 14, 0, 0)); // Note: Months are 0-indexed in JavaScript Date
+
+    for (const category of categories) {
+      const votes = await this.prismaService.userAttestation.findMany({
+        where: {
+          collectionId: category.id,
+          // createdAt: { lte: targetDate },
+        },
+      });
+
+      console.log('Category:', category.name);
+      console.log('Total votes:', votes.length);
+      console.log(
+        `Votes from recipients ${
+          votes.filter(
+            (el) =>
+              recipients.findIndex((user) => user.id === el.userId) !== -1,
+          ).length
+        }`,
+      );
+      console.log(
+        `Votes from holders ${
+          votes.filter(
+            (el) => holders.findIndex((user) => user.id === el.userId) !== -1,
+          ).length
+        }`,
+      );
+      console.log(
+        `Votes from badgeholders ${
+          votes.filter(
+            (el) =>
+              badgeHolders.findIndex((user) => user.id === el.userId) !== -1,
+          ).length
+        }`,
+      );
+      console.log(
+        `Votes from delegates ${
+          votes.filter(
+            (el) => delegates.findIndex((user) => user.id === el.userId) !== -1,
+          ).length
+        }`,
+      );
+      console.log();
+    }
+
+    return 'Success';
   }
 }
